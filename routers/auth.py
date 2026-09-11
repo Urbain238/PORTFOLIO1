c80f5542b3665ca6f278f8cc9d679e373e5a0a51
NYENDE NDJANA URBAIN <urbain.nyende@facsciences-uy1.cm>
2026-09-11 15:16:36 +0100

    Update auth.py

--- routers/auth.py
+++ routers/auth.py
@@ -1,1 +1,21 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from core.database import get_db
+from core.security import verify_password, create_access_token
+import models
 
+router = APIRouter()
+
+@router.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.username == form_data.username).first()
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Identifiants incorrects",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    access_token = create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
