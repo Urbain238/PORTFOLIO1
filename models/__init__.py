f4f4dc8d0cbb07947a8191dd27d62df8c0fcf0ab
NYENDE NDJANA URBAIN <urbain.nyende@facsciences-uy1.cm>
2026-09-11 15:04:53 +0100

    Import models in __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,8 @@
-
+from .user import User
+from .profile import Profile
+from .parcours import Parcours
+from .project import Project, ProjectImage
+from .gallery import GalleryItem
+from .cv import CurriculumVitae, CVExperience, CVEducation, CVSkill, CVLanguage
+from .contact import ContactInfo, ContactMessage
+from .secret import SecretVault
