e892e2468417e292ed0cee71a8e18acc228f50a4
NYENDE NDJANA URBAIN <urbain.nyende@facsciences-uy1.cm>
2026-09-11 15:27:01 +0100

    Create user.py

---
+++ shemas/user.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, EmailStr, ConfigDict
+from datetime import datetime
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+
+class UserResponse(UserBase):
+    id: int
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
